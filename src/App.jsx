59edd0d123b6b1497cfe874c11689e7f3fe93944
William Hortone <willhortone@gmail.com>
2024-01-22 02:38:42 +0800

    added the starting  settings

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>William Hortone</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,23 @@
+import doctorsImg from "../assets/doctor.png";
+import plusImg from "../assets/plus.png";
+import plusIcon from "../assets/plus-icon.png";
+import clockIcon from "../assets/clock.png";
+import docTool from "../assets/semiologia.png";
+import image1 from "../assets/image1.png";
+import image2 from "../assets/image2.png";
+import checkCircle from "../assets/check-circle.png";
+import ellipse from "../assets/Ellipse.png";
+import messageCircle from "../assets/message-circle.png";
+
+export {
+  doctorsImg,
+  plusIcon,
+  plusImg,
+  clockIcon,
+  docTool,
+  image1,
+  image2,
+  checkCircle,
+  ellipse,
+  messageCircle,
+};
